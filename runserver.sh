1df7963bc71161880bb535eb09d59a3a04322439
Kelly Bucey <kelly@bricksimple.com>
2014-04-12 13:59:31 -0400

    Updated shell scripts.

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source env/bin/activate
-./manage.py runserver --settings=gomoto.local_settings
+./manage.py runserver --settings=rfmedia.local_settings
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 source env/bin/activate
-./manage.py test --settings=gomoto.local_settings
+./manage.py test --settings=rfmedia.local_settings
