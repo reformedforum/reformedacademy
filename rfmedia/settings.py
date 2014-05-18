191ff0e5938b571fbcbc25f0c98003754248f956
Kelly Bucey <kelly@bricksimple.com>
2014-05-18 16:06:10 -0400

    Added secret key setting.

--- rfmedia/settings.py
+++ rfmedia/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY is an environment variable specified in the console
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['SECRET_KEY']
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
