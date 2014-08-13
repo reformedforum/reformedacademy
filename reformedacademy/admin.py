d13f62bf8d5effab3798f83ca5aab0f6d3573fc9
Kelly Bucey <kelly@bricksimple.com>
2014-08-12 23:02:22 -0400

    Added models
    Added models for Category, Course, Lesson, Task, and Teacher. Teacher has an image field, so Pillow was added as a requirement.
    Admin sections for Category, Course, and Lesson were added.

--- reformedacademy/admin.py
+++ reformedacademy/admin.py
@@ -5,5 +5,31 @@ Defines admin functionality for the reformedacademy app.
 Created by kabucey
 
 """
+from django.contrib import admin
+from reformedacademy import models
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = models.Category
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class CourseAdmin(admin.ModelAdmin):
+    model = models.Course
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class LessonAdmin(admin.ModelAdmin):
+    model = models.Lesson
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('__unicode__', 'course', 'order')
+    list_filter = ('course',)
+    list_editable = ('order',)
+    ordering = ('course', 'order')
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Course, CourseAdmin)
+admin.site.register(models.Lesson, LessonAdmin)
 
 
--- reformedacademy/models.py
+++ reformedacademy/models.py
@@ -7,9 +7,55 @@ Created by kabucey
 """
 from django.db import models
 from django.contrib.auth.models import User
+from rfmedia.models import Asset
 
 
 class ActivationKey(models.Model):
     user = models.ForeignKey(User)
     key = models.CharField(max_length=255)
     sent = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Course(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField()
+    order = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Task(models.Model):
+    lesson = models.ForeignKey(Lesson)
+    asset = models.ForeignKey(Asset, on_delete=models.PROTECT)
+    order = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return '{} {}'.format(self.lesson, self.asset.tag)
+
+
+class Teacher(models.Model):
+    course = models.ManyToManyField(Course)
+    name = models.CharField(max_length=255)
+    biography = models.TextField()
+    profile_image = models.ImageField(upload_to='profile_images/teachers')
