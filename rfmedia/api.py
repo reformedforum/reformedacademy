e27ba7319e45ccea2ee6439ef6f002f417768a7e
Kelly Bucey <kelly@bricksimple.com>
2014-08-14 22:36:13 -0400

    Added the GPLv3 license to the project.

--- rfmedia/api.py
+++ rfmedia/api.py
@@ -1,8 +1,22 @@
-"""rfmedia views.py
-
+"""
 Defines api resources for the rfmedia app.
 
-Created by kabucey
+Copyright (C) 2014 by Reformed Forum <reformedforum.org>
+
+This file is part of Reformed Academy.
+
+Reformed Academy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Reformed Academy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Reformed Academy.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 from tastypie.resources import ModelResource
--- rfmedia/models.py
+++ rfmedia/models.py
@@ -1,10 +1,25 @@
-"""rfmedia models.py
-
+"""
 Defines models for the rfmedia app.
 
-Created by kabucey
+Copyright (C) 2014 by Reformed Forum <reformedforum.org>
+
+This file is part of Reformed Academy.
+
+Reformed Academy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Reformed Academy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Reformed Academy.  If not, see <http://www.gnu.org/licenses/>.
 
 """
+
 from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
