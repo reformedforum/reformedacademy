d2930c3251effc2fde49d872a0187ae8680a3411
Nicholas Brawand <nicholasbrawand@gmail.com>
2014-09-02 20:47:02 -0500

    added correction to uncomplete_task

--- reformedacademy/models.py
+++ reformedacademy/models.py
@@ -239,7 +239,7 @@ class Task(models.Model):
     def uncomplete_task(self, user):
         """Deletes TaskProgress object from database"""
         if user.is_authenticated():
-        TaskProgress.objects.get(user__exact=user, task__exact=self).delete()
+            TaskProgress.objects.get(user__exact=user, task__exact=self).delete()
         return None
 
     class Meta:
