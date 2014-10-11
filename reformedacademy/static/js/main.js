434496ebc6220922472b6a6fd204419b33d62c7a
Kelly Bucey <kelly@bricksimple.com>
2014-10-11 17:50:29 -0400

    Fixed messages not displaying correctly.

--- reformedacademy/static/js/main.js
+++ reformedacademy/static/js/main.js
@@ -2,7 +2,4 @@ $(document).ready(function() {
     $("#logo").click(function() {
         $('.menu').toggleClass('open');
     });
-    {% for message in messages %}
-    toastr.{{ message.tags }}('{{ message }}')
-    {% endfor %}
 });
