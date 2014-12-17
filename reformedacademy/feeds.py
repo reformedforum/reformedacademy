5c28beb0a2c42cbfb44f47468bd53a6855ce67a2
Kel Bucey <kabucey@Kel-MBP-2.home>
2014-12-16 22:55:45 -0500

    Added image support to the course rss feed.
    Resolves #52

--- reformedacademy/feeds.py
+++ reformedacademy/feeds.py
@@ -3,14 +3,34 @@ import datetime
 from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
+from django.templatetags import static
 from django.utils.feedgenerator import Rss201rev2Feed
 from reformedacademy.models import Course, Task
 
 
+def static_url(request, path):
+    """Generates a static url based on the request and path."""
+    url = static.static(path)
+    if 'http' not in url:
+        url = 'http://{}{}'.format(request.get_host(), url)
+
+    return url
+
+
+class RFFeed(Rss201rev2Feed):
+    """Reformed Forum Feed."""
+    def add_root_elements(self, handler):
+        super(RFFeed, self).add_root_elements(handler)
+        handler.startElement('image', {})
+        handler.addQuickElement('url', self.feed['image_url'])
+        handler.addQuickElement('title', self.feed['title'])
+        handler.addQuickElement('link', self.feed['link'])
+        handler.endElement('image')
+
+
 class CourseFeed(Feed):
     """Defines a feed for a course."""
-
-    feed_type = Rss201rev2Feed
+    feed_type = RFFeed
 
     def __init__(self):
         self.request = None
@@ -20,6 +40,10 @@ class CourseFeed(Feed):
         self.request = request
         self.course = get_object_or_404(Course, slug=slug)
         return self.course
+
+    def feed_extra_kwargs(self, obj):
+        url = static_url(self.request, 'images/logo_bg.png')
+        return {'image_url': url}
 
     def title(self, obj):
         return obj.name
