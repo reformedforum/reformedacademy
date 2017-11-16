d65c9767e0664aeae4448f459c7fe4ef0ad32173
Kel Bucey <kel.bucey@ismconnect.io>
2017-11-15 21:27:38 -0500

    Fixed python 3 imports.

--- reformedacademy/staging_settings.py
+++ reformedacademy/staging_settings.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with Reformed Academy.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-from settings import *
+from .settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
